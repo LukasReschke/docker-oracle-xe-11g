7c8c9a13fb91f568910b573f06b92fff2d4e8f77
Lukas Reschke <lukas@owncloud.com>
2015-09-17 15:12:34 +0200

    Update tuning.sql

--- tuning.sql
+++ tuning.sql
@@ -4,7 +4,6 @@ alter system set job_queue_processes=0 scope=both;
 alter system set aq_tm_processes=0 scope=both;
 exec dbms_xdb.setlistenerlocalaccess(FALSE);
 exec dbms_xdb.sethttpport(0);
-ALTER SYSTEM SET recyclebin = OFF;
 alter system set sessions=250 scope=spfile;
 alter system set processes=250 scope=spfile;
 alter system set session_cached_cursors=250 scope=spfile;
