a65720afe2a9b08e95c76fe23e7361208071cb67
Lukas Reschke <lukas@owncloud.com>
2015-09-18 17:47:08 +0200

    Update tuning.sql

--- tuning.sql
+++ tuning.sql
@@ -2,8 +2,6 @@ alter system reset dispatchers scope=spfile sid='*';
 alter system reset shared_servers scope=spfile sid='*';
 alter system set job_queue_processes=0 scope=both;
 alter system set aq_tm_processes=0 scope=both;
-exec dbms_xdb.setlistenerlocalaccess(FALSE);
-exec dbms_xdb.sethttpport(0);
 alter system set sessions=250 scope=spfile;
 alter system set processes=250 scope=spfile;
 alter system set session_cached_cursors=250 scope=spfile;
